4515935f7df82da13cb682a2268e24808d1256d4
Julia van der Kris <juliavdkris@proton.me>
2024-10-22 23:39:12 +0200

    Compute & compare different distances to point clouds

--- data/tree 1.las
+++ data/tree 1.las
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:664bb9f0da164f9058a1b2e2a37dde0666fc8f28ef355be18e4622790726fd8a
-size 42155267
+oid sha256:9b5758d8115a77eafdf0c5c7f1280d82a818df8c062ba77382bbf586280313f7
+size 42147247
--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
 	with laspy.open('data/tree 1.las') as f:
 		las = f.read()
 		print('Points: ', las.points)
+		print('Dimensions:', list(las.header.point_format.dimension_names))
+
+		# xs = las.points.x
+		# ys = las.points.y
+		# zs = las.points.z
+		# bbox = las.header.min, las.header.max
 
 
 if __name__ == "__main__":
